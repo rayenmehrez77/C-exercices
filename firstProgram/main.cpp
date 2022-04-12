e64f38970048ac8b1499ec4033714e0612eba80c
rayen <rayenmehrez28@gmail.com>
2022-04-12 16:55:33 +0100

    Lesson1

--- firstProgram/main.cpp
+++ firstProgram/main.cpp
@@ -3,10 +3,24 @@ using namespace std;
 
 
 int main() {
-
-    cout << "Dear Sir\t Madam" << endl;
-
-
+    cout << "Size of char : " << sizeof(char)
+         << " byte" << endl;
+    cout << "Size of int : " << sizeof(int)
+         << " bytes" << endl;
+    cout << "Size of short int : " << sizeof(short int)
+         << " bytes" << endl;
+    cout << "Size of long int : " << sizeof(long int)
+         << " bytes" << endl;
+    cout << "Size of signed long int : " << sizeof(signed long int)
+         << " bytes" << endl;
+    cout << "Size of unsigned long int : " << sizeof(unsigned long int)
+         << " bytes" << endl;
+    cout << "Size of float : " << sizeof(float)
+         << " bytes" <<endl;
+    cout << "Size of double : " << sizeof(double)
+         << " bytes" << endl;
+    cout << "Size of wchar_t : " << sizeof(wchar_t)
+         << " bytes" <<endl;
 
     return 0;
 }
