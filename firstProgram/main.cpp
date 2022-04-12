a3c231b495542798fedd9b1244a77f4b15f729cb
rayen <rayenmehrez28@gmail.com>
2022-04-12 16:22:45 +0100

    data types & Range

--- firstProgram/main.cpp
+++ firstProgram/main.cpp
@@ -3,64 +3,24 @@ using namespace std;
 
 
 int main() {
-    string name;
-    int age;
-    string city, country;
-    int monthlySalary, yearlySalary;
-    char gender;
-    bool isMarried;
-    int num1, num2, num3;
-
-    cout << "How old are you ?" << endl;
-    cin >> age;
-
-    cout << "After 5 years you will be " << age + 5 << endl;
-
-
-    cin >> num1;
-    cout << " + " << endl;
-    cin >> num2;
-    cout << " + " << endl;
-    cin >> num3;
-    cout << "----------------------------" << endl;
-
-    cout << "Total = " << num1 + num2 + num3 << endl;
-
-    cout << "Name: ";
-    cin >> name;
-
-    cout << "Age: ";
-    cin >> age;
-
-    cout << "City: ";
-    cin >> city;
-
-    cout << "Country: ";
-    cin >> country;
-
-    cout << "Monthly salary: ";
-    cin >> monthlySalary;
-
-    cout << "Yearly Salary: ";
-    cin >> yearlySalary;
-
-    cout << "Gender: ";
-    cin >> gender;
-
-    cout << "isMarried: true or false ";
-    cin >> isMarried;
-
-    cout << "*****************************" << endl;
-    cout << "Name: " << name << endl;
-    cout << "Age: " << age << " Years " << endl;
-    cout << "City: " << city << endl;
-    cout << "Country: " << country << endl;
-    cout << "Monthly Salary: " << monthlySalary << endl;
-    cout << "Yearly Salary: " << yearlySalary << endl;
-    cout << "Gender: " << gender << endl;
-    cout << "Married: " << isMarried << endl;
-    cout << "*****************************" << endl;
-
+    cout << "Size of char : " << sizeof(char)
+         << " byte" << endl;
+    cout << "Size of int : " << sizeof(int)
+         << " bytes" << endl;
+    cout << "Size of short int : " << sizeof(short int)
+         << " bytes" << endl;
+    cout << "Size of long int : " << sizeof(long int)
+         << " bytes" << endl;
+    cout << "Size of signed long int : " << sizeof(signed long int)
+         << " bytes" << endl;
+    cout << "Size of unsigned long int : " << sizeof(unsigned long int)
+         << " bytes" << endl;
+    cout << "Size of float : " << sizeof(float)
+         << " bytes" <<endl;
+    cout << "Size of double : " << sizeof(double)
+         << " bytes" << endl;
+    cout << "Size of wchar_t : " << sizeof(wchar_t)
+         << " bytes" <<endl;
     return 0;
 }
 
